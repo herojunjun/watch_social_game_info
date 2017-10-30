a0689fa40448e33521a904692947acbe3319eec7
junki.hiroi <junki.hiroi@drecom.co.jp>
2017-10-31 08:47:37 +0900

    通知する

--- main.rb
+++ main.rb
@@ -1,4 +1,19 @@
 require 'nokogiri'
+require 'yaml'
+require 'CGI'
+
+CONFIG_YAML = 'config.yml'
+
+def notice(data)
+  config = YAML.load_file(CONFIG_YAML)
+  text = CGI.escape("#{data[:title]} : #{data[:uri]}")
+  post_api = ["https://slack.com/api/chat.postMessage?",
+    "token=", config['token'],
+    "&channel=", config['channel'],
+    "&text=", text].join('')
+  p post_api
+  %x(curl "#{post_api}")
+end
 
 HOST_URI = "http://gamebiz.jp"
 NEW_RELEASE_URI = "http://gamebiz.jp/article/category/5"
@@ -11,5 +26,5 @@ doc.xpath('//div[@class="Article_detail"]/h2/a').each do |node|
       :title => node.inner_text,
       :uri => (HOST_URI + node.attribute('href').value)
   }
-  p data
+  notice(data)
 end
